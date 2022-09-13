a9e1acc7e09245cc8f7c95af1b6157c2d95fdaf4
louis-br <46930119+louis-br@users.noreply.github.com>
2022-09-13 14:57:33 -0300

    Add Message

--- Main.java
+++ Main.java
@@ -1,70 +1,146 @@
 import java.net.*;
 import java.io.*;
+import java.nio.ByteBuffer;
+import java.nio.charset.Charset;
+import java.util.Arrays;
 
 public class Main {
     public static void main(String args[]) {
         System.out.println("Main");
-        //Peer p = new Peer(args[0]);
-        //p.multiCastSend(args[1]);
-        Process process = new Process(args[0]);
+        // Peer p = new Peer(args[0]);
+        // p.multiCastSend(args[1]);
+        //Collaborator collaborator = new Collaborator(args[0]);
+        Message message = new Message(12345, false, true, false, "Olá");
+        message.print();
+        message.encode();
+        message.decode(message.bytes);
+        message.print();
     }
 }
 
-class Process extends Thread {
-    Peer ListeningGroup;
+class Collaborator extends Thread {
+    Peer Group;
 
-    public Process(String addr) {
-        ListeningGroup = new Peer(addr);
+    public Collaborator(String addr) {
+        Group = new Peer(addr);
         this.start();
     }
 
     public void run() {
         while (true) {
-            //ListeningGroup.multiCastSend("Ola");
-            ListeningGroup.multiCastListen();
+            // ListeningGroup.multiCastSend("Ola");
+            //Group.multiCastListen();
         }
     }
 }
 
 class Coordenator {
-    
+
 }
 
-class Peer{
-    MulticastSocket s = null;
-    NetworkInterface netIf;
-    InetSocketAddress g;
+class Message {
+    int sourceID = 0;
+    boolean isCoordenator = false;
+    boolean isElection = false;
+    boolean isReply = false;
+    String body = "";
+    byte[] bytes;
 
-    public Peer(String addr){
-        try{
+    public Message() {
+        bytes = new byte[1000];
+    }
+    
+    public Message(int id) {
+        this();
+        sourceID = id;
+    }
+    
+    public Message(int id, boolean election, boolean reply, boolean coordenator, String message) {
+        this(id);
+        isElection = election;
+        isReply = reply;
+        isCoordenator = coordenator;
+        body = message;
+    }
+
+    public Message(byte[] payload) {
+        decode(payload);
+    }
+
+    public byte[] encode() {
+        byte flags = (byte) ((isCoordenator ? 1<<0 : 0) +
+                            (isElection     ? 1<<1 : 0) +
+                            (isReply        ? 1<<2 : 0));
+                    
+        System.out.println("Flags: " + flags);
+        
+        ByteBuffer buffer = ByteBuffer.wrap(bytes);
+        buffer.put(flags)
+              .putInt(sourceID)
+              .put(body.getBytes(Charset.forName("UTF-8")));
+        
+        return bytes;
+    }
+
+    public void decode(byte[] payload) {
+        ByteBuffer buffer = ByteBuffer.wrap(payload);
+
+        byte flags = buffer.get();
+        isCoordenator = (flags & 1<<0) > 0;
+        isElection =    (flags & 1<<1) > 0;
+        isReply =       (flags & 1<<2) > 0;
+        sourceID = buffer.getInt();
+        body = new String(
+            Arrays.copyOfRange(payload, buffer.position(), payload.length),
+            Charset.forName("UTF-8"));
+
+        System.out.println("Buffer pos: " + buffer.position() + " Length: " + payload.length);
+    }
+
+    public void print() {
+        System.out.println("isCoordenator: " + isCoordenator);
+        System.out.println("isElection: " + isElection);
+        System.out.println("isReply: " + isReply);
+        System.out.println("sourceID: " + sourceID);
+        System.out.println("body: " + body);
+    }
+}
+
+class Peer {
+    MulticastSocket socket = null;
+    NetworkInterface networkInterface;
+    InetSocketAddress address;
+
+    public Peer(String addr) {
+        try {
             InetAddress group = InetAddress.getByName(addr);
-            s = new MulticastSocket(6789);
-            g = new InetSocketAddress(group, 6789);
-            netIf = NetworkInterface.getByName("le0");
-            s.joinGroup(g, netIf);
-        }catch (SocketException e){System.out.println("Socket 42: " + e.getMessage());
-		}catch (IOException e){System.out.println("IO: " + e.getMessage());
+            socket = new MulticastSocket(6789);
+            address = new InetSocketAddress(group, 6789);
+            networkInterface = NetworkInterface.getByName("le0");
+            socket.joinGroup(address, networkInterface);
+        } catch (IOException e) { e.printStackTrace();
         }
     }
 
-    public void multiCastSend(String message){
-        try{
-            byte [] m = message.getBytes();
-            DatagramPacket messageOut = new DatagramPacket(m, m.length, g);
-            s.send(messageOut);	
-        }catch (SocketException e){System.out.println("Socket 52: " + e.getMessage());
-		}catch (IOException e){System.out.println("IO: " + e.getMessage());
+    public void multiCastSend(Message message) {
+        try {
+            message.encode();
+            DatagramPacket messageOut = new DatagramPacket(message.bytes, message.bytes.length, address);
+            socket.send(messageOut);
+        } catch (IOException e) { e.printStackTrace();
         }
     }
 
-    public void multiCastListen(){
-        try{
-            byte[] buffer = new byte[1000];
-            DatagramPacket messageIn = new DatagramPacket(buffer, buffer.length);
-            s.receive(messageIn);
+    public Message multiCastListen() {
+        Message message = new Message();
+        try {
+            //byte[] buffer = new byte[1000];
+            DatagramPacket messageIn = new DatagramPacket(message.bytes, message.bytes.length);
+            socket.receive(messageIn);
             System.out.println("Received:" + new String(messageIn.getData()));
-        }catch (SocketException e){System.out.println("Socket 52: " + e.getMessage());
-		}catch (IOException e){System.out.println("IO: " + e.getMessage());
+            return message;
+        } catch (IOException e) { e.printStackTrace();
         }
+        return message;
     }
 }
